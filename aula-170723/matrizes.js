7fe8f04ea3990cad7abcefffc247633da3fe5b10
felipe-laudano <felipelaudano@hotmail.com>
2023-07-17 19:02:04 -0300

    Alterações nos arquivos da aula 17/07/2023

--- aula-170723/matrizes.js
+++ aula-170723/matrizes.js
@@ -27,3 +27,9 @@ const matriz3x3 = [
     [6, 5, 9], 
     [2, 3, 7]
 ]
+
+for(let x = 0; x < matriz3x3.length; x++) {
+    for(let y = 0; y < matriz3x3[x].length; y++) {
+        console.log(matriz3x3[x][y]);
+    }
+}
