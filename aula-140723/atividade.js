cf64355076186a730758b82b15c71b7382f721ee
felipe-laudano <felipelaudano@hotmail.com>
2023-07-17 19:09:52 -0300

    Alterações na atividade da aula 14/07/2023

--- aula-140723/atividade.js
+++ aula-140723/atividade.js
@@ -13,15 +13,44 @@ function concString(callback) {
 concString(palavras => palavras.join(""));
 
 //Crie uma função que receba um vetor de números e retorne o maior valor presente no vetor.
+const numeros = [1, 2, 6, 9, 3, 0, 7, 8, 4, 5];
+function maiorDoVetor(numeros) {
+    let maior = numeros[0];
+    for(let n of numeros) {
+        if(n > maior) {
+            maior = n;
+        }
+    }
+    return maior;
+}
+console.log("O maior numero do vetor é " + maiorDoVetor(numeros));
+
 //Crie uma função que receba um vetor de números e retorne a média dos elementos.
+function mediaDoVetor(numeros) {
+    let total = 0;
+    let tamanho = numeros.length;
+    for(let n of numeros) {
+        total += n;
+    }
+    return total/tamanho; // media
+}
+console.log("A média dos numeros do vetor é " + mediaDoVetor(numeros));
+
 //Dado um array de números, utilize o método map para retornar um novo array com cada número elevado a 2.
+console.log(numeros.map(n => n**2));
+
 //Dado um array de palavras, utilize o método filter para retornar um novo array contendo apenas as palavras que começam com a letra 'A'.
+const palavras = ["Gato", "Anel", "Banana", "Computador", "Sol", "Casa", "Carro", "Arvore", "Livro", "Chocolate"];
+console.log(palavras.filter(p => (p[0] == "A")));
+
 //Dado um array de números, utilize o método forEach para imprimir no console cada número multiplicado por 3.
+numeros.forEach(n => console.log(`${n} x 3 = ${n*3}`));
+
 //Dado um array de números, utilize o método includes para verificar se o número 10 está presente no array.
+console.log(numeros.includes(10));
+
 
 //Correção 
-
-const numeros = [1, 2, 6, 9, 3, 0, 7, 8, 4, 5];
 
 // Crie uma função que receba um vetor de números e retorne a soma de todos os elementos.
 // COM FOR EACH
@@ -78,7 +107,6 @@ console.log(numeros.map(n => n**2));
 
 
 // Dado um array de palavras, utilize o método filter para retornar um novo array contendo apenas as palavras que começam com a letra 'A'.
-const palavras = ["Gato", "Anel", "Banana", "Computador", "Sol", "Casa", "Carro", "Arvore", "Livro", "Chocolate"];
 console.log(palavras.filter(p => (p[0] == "A")));
 
 
